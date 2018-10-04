1038743ae556c7f27cf21631fda6a377d1eed906
Mateusz Boczarski <matbocz@outlook.com>
2018-10-04 15:48:39 +0200

    Create Second.java

---
+++ src/lab1/Second.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Second {
+    public static void main(String[] args) {
+        int a=8, b=3;
+        int wynik1, wynik2, wynik3, wynik4, wynik5;
+
+        wynik1=a+b;
+        wynik2=a-b;
+        wynik3=a*b;
+        wynik4=a/b;
+        wynik5=a%b;
+
+        System.out.printf("%d + %d = %d \n",a,b,wynik1);
+        System.out.printf("%d - %d = %d \n",a,b,wynik2);
+        System.out.printf("%d * %d = %d \n",a,b,wynik3);
+        System.out.printf("%d / %d = %d \n",a,b,wynik4);
+        System.out.printf("%d mod %d = %d \n",a,b,wynik5);
+    }
+}
