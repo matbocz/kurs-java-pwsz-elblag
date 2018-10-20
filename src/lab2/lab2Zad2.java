68878e120f15c6042023a8d81b356ef09a5560d7
Mateusz Boczarski <matbocz@outlook.com>
2018-10-20 11:30:35 +0200

    comments - minor improvements

--- src/lab2/lab2Zad2.java
+++ src/lab2/lab2Zad2.java
@@ -12,25 +12,25 @@ public class lab2Zad2 { //definicja klasy publicznej
         //utworzenie obiektu typu random
         Random r = new Random();
 
-        //wydrukowanie liczb losowych
+        //wydrukowanie liczb losowych typu int
         System.out.println("5 losowych liczb typu int:");
         for (i = 0; i < 5; i++)
             System.out.println(r.nextInt());
         System.out.printf("\n");
 
-        //wydrukowanie liczb losowych
+        //wydrukowanie liczb losowych typu float
         System.out.println("10 losowych liczb typu float:");
         for (i = 0; i < 10; i++)
             System.out.println(r.nextFloat());
         System.out.printf("\n");
 
-        //wydrukowanie liczb losowych
+        //wydrukowanie liczb losowych typu int z zakresu [0,N)
         System.out.println("12 losowych liczb typu int z zakresu [0,N):");
         for (i = 0; i < 12; i++)
             System.out.println(r.nextInt(N));
         System.out.printf("\n");
 
-        //wydrukowanie liczb losowych
+        //wydrukowanie liczb losowych typu int z zakresu [-10,20]
         System.out.println("15 losowych liczb typu int z zakresu [-10,20]:");
         for (i = 0; i < 15; i++) {
             a = r.nextInt(31) - 10;
