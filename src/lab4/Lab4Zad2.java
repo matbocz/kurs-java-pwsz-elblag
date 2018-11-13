5a3f50fb6f87b29de95be2af1395dba4df8e9f96
Mateusz Boczarski <matbocz@outlook.com>
2018-11-13 18:42:20 +0100

    Reformat code

--- src/lab4/Lab4Zad2.java
+++ src/lab4/Lab4Zad2.java
@@ -2,10 +2,11 @@ package lab4;
 
 public class Lab4Zad2 {
     public static void main(String[] args) {
-        int x[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        int x[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
         int wyniki[];
 
-        wyniki=dimensions(x);
+        wyniki = dimensions(x);
+
         System.out.println("Ilość wierszy: " + wyniki[0]);
         System.out.println("Ilość kolumn: " + wyniki[1]);
     }
@@ -15,11 +16,11 @@ public class Lab4Zad2 {
         int wyniki[] = new int[2];
         int iloscWierszy, iloscKolumn;
 
-        iloscWierszy=tab.length;
-        iloscKolumn=tab[0].length;
+        iloscWierszy = tab.length;
+        iloscKolumn = tab[0].length;
 
-        wyniki[0]=iloscWierszy;
-        wyniki[1]=iloscKolumn;
+        wyniki[0] = iloscWierszy;
+        wyniki[1] = iloscKolumn;
 
         return wyniki;
     }
