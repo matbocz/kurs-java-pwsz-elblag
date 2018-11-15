218bed4cb0f97b98041599af0bc8cd4edfd461d3
Mateusz Boczarski <matbocz@outlook.com>
2018-11-15 15:26:37 +0100

    Create src\lab4\Lab4Zad3.java

---
+++ src/lab4/Lab4Zad3.java
@@ -0,0 +1,12 @@
+package lab4;
+
+import java.math.BigInteger;
+
+public class Lab4Zad3 {
+    public static void main(String[] args) {
+        //utworzenie obiektu typu BigInteger za pomocą konstruktora
+        BigInteger a = new BigInteger("2345768907654222");
+        //utworzenie obiektu typu BigInteger za pomocą metody valueOf
+        BigInteger b = BigInteger.valueOf(3000);
+    }
+}
