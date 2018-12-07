122efddeb3fe5de515c7f0a5feaedc0a1828e4e0
Mateusz Boczarski <matbocz@outlook.com>
2018-12-07 13:51:34 +0100

    Update convertToBinary method
    32 bits instead of 8 bits.

--- src/lab5/Lab5Zad2.java
+++ src/lab5/Lab5Zad2.java
@@ -21,15 +21,15 @@ public class Lab5Zad2 {
     //metoda realizująca konwersję liczby dziesiętnej na postać binarną
     static void converToBinary(int x) {
         int k = 1;
-        int bit[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int bit[] = new int[32];
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 32; i++) {
             if ((x & k) != 0) bit[i] = 1;
             else bit[i] = 0;
             k = k * 2;
         }
 
-        for (int i = 7; i >= 0; i--) System.out.printf("%d ", bit[i]);
-        System.out.println("\n");
+        for (int i = 31; i >= 0; i--) System.out.printf("%d ", bit[i]);
+        System.out.println();
     }
 }
