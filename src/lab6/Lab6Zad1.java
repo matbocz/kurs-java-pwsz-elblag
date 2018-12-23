d15fed0c46742401ca634e7b7ead2b035ea5cdd9
Mateusz Boczarski <matbocz@outlook.com>
2018-12-23 16:43:18 +0100

    Create src\lab6\Lab6Zad1.java

---
+++ src/lab6/Lab6Zad1.java
@@ -0,0 +1,23 @@
+package lab6;
+
+import java.util.Date;
+
+public class Lab6Zad1 {
+    public static void main(String[] args) {
+        //utworzenie dwóch obiektów klasy String
+        String s1 = new String("Mateusz ");
+        String s2 = new String("Boczarski");
+
+        //połączenie dwóch obiektów klasy String i wydrukowanie
+        String s3 = s1 + s2;
+        System.out.println(s3);
+        System.out.println();
+
+        //utworzenie obiektu klasy Date
+        Date data = new Date();
+
+        //utworzenie obiektu klasy String i wydrukowanie
+        String s4 = "Dzisiaj jest: " + data;
+        System.out.println(s4);
+    }
+}
