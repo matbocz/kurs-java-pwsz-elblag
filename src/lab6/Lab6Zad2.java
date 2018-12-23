3d3bf58352ebd18e4e2b50672f04320fda3ae74c
Mateusz Boczarski <matbocz@outlook.com>
2018-12-23 16:58:55 +0100

    Create src\lab6\Lab6Zad2.java

---
+++ src/lab6/Lab6Zad2.java
@@ -0,0 +1,25 @@
+package lab6;
+
+public class Lab6Zad2 {
+    public static void main(String[] args) {
+        //utworzenie dwóch obiektów klasy String
+        String s1 = new String("Mateusz");
+        String s2 = new String("Boczarski");
+
+        //porównanie obiektów klasy String za pomocą metody equals i wydrukowanie
+        boolean w1 = s1.equals(s2);
+        System.out.println(w1);
+
+        //porównanie obiektów klasy String za pomocą metody equalsIgnoreCase i wydrukowanie
+        boolean w2 = s1.equalsIgnoreCase(s2);
+        System.out.println(w2);
+
+        //porównanie obiektów klasy String za pomocą metody compareTo i wydrukowanie
+        int w3 = s1.compareTo(s2);
+        System.out.println(w3);
+
+        //porównanie obiektów klasy String za pomocą metody compareToIgnoreCase i wydrukowanie
+        int w4 = s1.compareToIgnoreCase(s2);
+        System.out.println(w4);
+    }
+}
