720acc97833cbb3017b390375a7cb32d1200173d
Mateusz Boczarski <matbocz@outlook.com>
2018-12-23 17:56:40 +0100

    Create src\lab6\Lab6Zad5.java

---
+++ src/lab6/Lab6Zad5.java
@@ -0,0 +1,12 @@
+package lab6;
+
+public class Lab6Zad5 {
+    public static void main(String[] args) {
+        //utworzenie obiektu klasy String
+        String s1 = new String("  Kowalski  ");
+
+        //usunięcie zbędnych spacji z łańcucha i wydrukowanie
+        String s2 = s1.trim();
+        System.out.println(s2);
+    }
+}
