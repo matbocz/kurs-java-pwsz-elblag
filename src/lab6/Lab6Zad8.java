beeaa6b9635cac6271ffcb24ace2439d8dace8ac
Mateusz Boczarski <matbocz@outlook.com>
2018-12-23 18:39:46 +0100

    Create src\lab6\Lab6Zad8.java

---
+++ src/lab6/Lab6Zad8.java
@@ -0,0 +1,12 @@
+package lab6;
+
+public class Lab6Zad8 {
+    public static void main(String[] args) {
+        //utworzenie obiektu klasy String
+        String str = "Mateusz";
+
+        //wydobycie podciągu z łańcucha i wydrukowanie
+        String subStr = str.substring(1, 4);
+        System.out.println(subStr);
+    }
+}
