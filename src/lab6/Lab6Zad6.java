209ca460dbceba63455c9bbfd1dddd05baa49319
Mateusz Boczarski <matbocz@outlook.com>
2018-12-23 18:01:59 +0100

    Create src\lab6\Lab6Zad6.java

---
+++ src/lab6/Lab6Zad6.java
@@ -0,0 +1,13 @@
+package lab6;
+
+public class Lab6Zad6 {
+    public static void main(String[] args) {
+        //utworzenie obiektu klasy String
+        String str = "zielony,niebieski,czerwony";
+
+        //podzielenie łańcucha na części i wydrukowanie
+        String results[] = str.split(",");
+        for(String x: results)
+            System.out.println(x);
+    }
+}
