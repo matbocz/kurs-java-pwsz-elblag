8ba285db71cbe15c764dc4baa047676753d9b228
Mateusz Boczarski <matbocz@outlook.com>
2019-01-26 19:12:42 +0100

    Create src\lab10\Lab10Zad1.java

---
+++ src/lab10/Lab10Zad1.java
@@ -0,0 +1,79 @@
+package lab10;
+
+interface Obliczenia {
+    double poleTrojkata();
+
+    double poleKwadratu();
+
+    double poleProstokata();
+
+    double poleTrapezu();
+}
+
+public class Lab10Zad1 {
+    public static void main(String[] args) {
+        Trojkat trojkat = new Trojkat();
+        Kwadrat kwadrat = new Kwadrat();
+        Prostokat prostokat = new Prostokat();
+        Trapez trapez = new Trapez();
+
+        System.out.println("Pole trójkąta: " + trojkat.poleTrojkata(2, 4));
+        System.out.println("Pole kwadratu: " + kwadrat.poleKwadratu(5));
+        System.out.println("Pole prostokąta: " + prostokat.poleProstokata(2, 6));
+        System.out.println("Pole trapezu: " + trapez.poleTrapezu(5, 5, 2));
+    }
+}
+
+abstract class Adapt implements Obliczenia {
+    public double poleTrojkata() {
+        return 0;
+    }
+
+    public double poleKwadratu() {
+        return 0;
+    }
+
+    public double poleProstokata() {
+        return 0;
+    }
+
+    public double poleTrapezu() {
+        return 0;
+    }
+}
+
+class Trojkat extends Adapt {
+    Trojkat() {
+    }
+
+    public double poleTrojkata(double a, double h) {
+        return (a * h) / 2;
+    }
+}
+
+class Kwadrat extends Adapt {
+    Kwadrat() {
+    }
+
+    public double poleKwadratu(double a) {
+        return a * a;
+    }
+}
+
+class Prostokat extends Adapt {
+    Prostokat() {
+    }
+
+    public double poleProstokata(double a, double b) {
+        return a * b;
+    }
+}
+
+class Trapez extends Adapt {
+    Trapez() {
+    }
+
+    public double poleTrapezu(double a, double b, double h) {
+        return ((a + b) * h) / 2;
+    }
+}
